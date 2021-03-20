b40c0c8eeb10316201dbf33b7fe6d5f40b2902a7
Fedor Korotkiy <prime@yandex-team.ru>
2021-03-20 14:21:03 +0000

    export public files

--- fileleak/fileleak_test.go
+++ fileleak/fileleak_test.go
@@ -4,12 +4,15 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
+	"syscall"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
 	"gitlab.com/slon/shad-go/fileleak"
+	"gitlab.com/slon/shad-go/tools/testtool"
 )
 
 type fakeT struct {
@@ -75,5 +78,65 @@ func TestFileLeak_ReopenFile(t *testing.T) {
 		ff, err := ioutil.TempFile("", "")
 		require.NoError(t, err)
 		f = ff
+	})
+}
+
+func TestFileLeak_PipeLeak(t *testing.T) {
+	checkLeak(t, true, func() {
+		f, _, err := os.Pipe()
+		require.NoError(t, err)
+		_ = f.Close()
+	})
+}
+
+func TestFileLeak_PipeNoLeak(t *testing.T) {
+	checkLeak(t, false, func() {
+		f, w, err := os.Pipe()
+		require.NoError(t, err)
+		_, _ = f.Close(), w.Close()
+	})
+}
+
+func TestFileLeak_SocketLeak(t *testing.T) {
+	var conn net.Listener
+	defer func() { conn.Close() }()
+
+	checkLeak(t, true, func() {
+		addr, err := testtool.GetFreePort()
+		require.NoError(t, err)
+
+		conn, err = net.Listen("tcp", fmt.Sprintf(":%s", addr))
+		require.NoError(t, err)
+	})
+}
+
+func TestFileLeak_SocketNoLeak(t *testing.T) {
+	checkLeak(t, false, func() {
+		addr, err := testtool.GetFreePort()
+		require.NoError(t, err)
+
+		conn, err := net.Listen("tcp", fmt.Sprintf(":%s", addr))
+		require.NoError(t, err)
+		_ = conn.Close()
+	})
+}
+
+func TestFileLeak_DupLeak(t *testing.T) {
+	var fd int
+	defer syscall.Close(fd)
+
+	checkLeak(t, true, func() {
+		var err error
+		fd, err = syscall.Dup(1)
+		require.NoError(t, err)
+
+	})
+}
+
+func TestFileLeak_DupNoLeak(t *testing.T) {
+	checkLeak(t, false, func() {
+		fd, err := syscall.Dup(1)
+		require.NoError(t, err)
+		_ = syscall.Close(fd)
 	})
 }
