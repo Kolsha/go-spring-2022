9f07a1b09a9983dc0561816b32aff9d493cbb349
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-02-28 20:50:32 +0700

    Solved utf8 again

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -3,6 +3,28 @@
 
 package reverse
 
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+//func ReverseOld(input string) string {
+//	runes := []rune(input)
+//	for l, r := 0, len(runes)-1; l < r; l, r = l+1, r-1 {
+//		runes[l], runes[r] = runes[r], runes[l]
+//	}
+//	return string(runes)
+//}
+
 func Reverse(input string) string {
-	return ""
+	var sb strings.Builder
+	sb.Grow(len(input))
+
+	for len(input) > 0 {
+		r, size := utf8.DecodeLastRuneInString(input)
+		sb.WriteRune(r)
+		input = input[:len(input)-size]
+	}
+
+	return sb.String()
 }
--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -3,6 +3,22 @@
 
 package spacecollapse
 
+import "unicode"
+
 func CollapseSpaces(input string) string {
-	return ""
+	runes := []rune(input)
+	prevSpace := false
+	l := 0
+	for r := 0; r < len(runes); r++ {
+		isSpace := unicode.IsSpace(runes[r])
+		if !isSpace {
+			runes[l] = runes[r]
+			l++
+		} else if !prevSpace {
+			runes[l] = ' '
+			l++
+		}
+		prevSpace = isSpace
+	}
+	return string(runes[:l])
 }
