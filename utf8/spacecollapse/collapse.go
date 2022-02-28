fa18cec0be5c6fdf0006f619e1199df0127c5a0e
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-02-28 21:14:24 +0700

    Solved collapse spaces

--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -3,22 +3,48 @@
 
 package spacecollapse
 
-import "unicode"
+import (
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
+//func CollapseSpaces(input string) string {
+//	runes := []rune(input)
+//	prevSpace := false
+//	l := 0
+//	for r := 0; r < len(runes); r++ {
+//		isSpace := unicode.IsSpace(runes[r])
+//		if !isSpace {
+//			runes[l] = runes[r]
+//			l++
+//		} else if !prevSpace {
+//			runes[l] = ' '
+//			l++
+//		}
+//		prevSpace = isSpace
+//	}
+//	return string(runes[:l])
+//}
 
 func CollapseSpaces(input string) string {
-	runes := []rune(input)
+	var sb strings.Builder
+	sb.Grow(len(input))
+
 	prevSpace := false
-	l := 0
-	for r := 0; r < len(runes); r++ {
-		isSpace := unicode.IsSpace(runes[r])
+	for len(input) > 0 {
+		r, size := utf8.DecodeRuneInString(input)
+		input = input[size:]
+
+		isSpace := unicode.IsSpace(r)
 		if !isSpace {
-			runes[l] = runes[r]
-			l++
+			sb.WriteRune(r)
 		} else if !prevSpace {
-			runes[l] = ' '
-			l++
+			sb.WriteRune(' ')
 		}
 		prevSpace = isSpace
+
 	}
-	return string(runes[:l])
+
+	return sb.String()
 }
