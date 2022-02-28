70a1e740d871164ef8e2a37fd23d0b820df8712b
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-02-28 15:32:29 +0700

    Solved hogwarts

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -3,6 +3,30 @@
 
 package hogwarts
 
+func learn(res []string, course string, prereqs map[string][]string, learned map[string]bool, pk map[string]bool) []string {
+	if pk[course] {
+		panic("a cycle")
+	}
+	if learned[course] {
+		return res
+	}
+
+	pk[course] = true
+
+	for _, prereq := range prereqs[course] {
+		res = learn(res, prereq, prereqs, learned, pk)
+	}
+	learned[course] = true
+	pk[course] = false
+	res = append(res, course)
+	return res
+}
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	learned := make(map[string]bool)
+	pk := make(map[string]bool)
+	res := make([]string, 0)
+	for course := range prereqs {
+		res = learn(res, course, prereqs, learned, pk)
+	}
+	return res
 }
