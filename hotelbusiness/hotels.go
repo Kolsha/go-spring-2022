e8ea187744049feb280fe71f4d539e51e3fcd5fb
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-02-28 13:55:45 +0700

    Solved hotels

--- hotelbusiness/hotels.go
+++ hotelbusiness/hotels.go
@@ -13,6 +13,37 @@ type Load struct {
 	GuestCount int
 }
 
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	if len(guests) == 0 {
+		return nil
+	}
+
+	maxOut := guests[0].CheckOutDate
+	for _, g := range guests {
+		maxOut = max(maxOut, g.CheckOutDate)
+	}
+
+	table := make([]int, maxOut+1)
+	for _, g := range guests {
+		table[g.CheckInDate]++
+		table[g.CheckOutDate]--
+	}
+
+	res := make([]Load, 0)
+
+	count := 0
+	for date, delta := range table {
+		count += delta
+		if delta == 0 {
+			continue
+		}
+		res = append(res, Load{StartDate: date, GuestCount: count})
+	}
+	return res
 }
