41c429a007067ba8dc724df7d69ccf1b0ba82fe5
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-03-01 15:46:35 +0700

    Solved wordcount

--- wordcount/main.go
+++ wordcount/main.go
@@ -3,6 +3,34 @@
 
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	counter := make(map[string]uint)
+
+	for _, filePath := range os.Args[1:] {
+		file, err := os.Open(filePath)
+		if err != nil {
+			continue
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := scanner.Text()
+			counter[line]++
+		}
+
+	}
+
+	for line, count := range counter {
+		if count < 2 {
+			continue
+		}
+		fmt.Printf("%v\t%v\n", count, line)
+	}
 }
