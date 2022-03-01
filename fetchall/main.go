e2377f240cb5534b720261d11c7fd56b5ef94570
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-03-01 16:39:19 +0700

    Solved fetchall

--- fetchall/main.go
+++ fetchall/main.go
@@ -3,6 +3,37 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
+func fetch(url string, ch chan<- time.Duration) {
+	start := time.Now()
+
+	resp, err := http.Get(url)
+
+	if err == nil {
+		defer resp.Body.Close()
+	}
+	ch <- time.Since(start)
+}
+
+func main() {
+	start := time.Now()
+
+	ch := make(chan time.Duration)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	close(ch)
+
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
 }
