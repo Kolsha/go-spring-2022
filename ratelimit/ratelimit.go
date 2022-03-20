2274e8f4ea1aa0ae07c258fa846fccc3c070c599
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-03-20 14:13:43 +0700

    Solved ratelimit

--- ratelimit/ratelimit.go
+++ ratelimit/ratelimit.go
@@ -11,6 +11,9 @@ import (
 
 // Limiter is precise rate limiter with context support.
 type Limiter struct {
+	buckets  chan struct{}
+	interval time.Duration
+	stop     chan struct{}
 }
 
 var ErrStopped = errors.New("limiter stopped")
@@ -18,13 +21,37 @@ var ErrStopped = errors.New("limiter stopped")
 // NewLimiter returns limiter that throttles rate of successful Acquire() calls
 // to maxSize events at any given interval.
 func NewLimiter(maxCount int, interval time.Duration) *Limiter {
-	panic("not implemented")
+	return &Limiter{
+		buckets:  make(chan struct{}, maxCount),
+		interval: interval,
+		stop:     make(chan struct{}, 1),
+	}
 }
 
 func (l *Limiter) Acquire(ctx context.Context) error {
-	panic("not implemented")
+	select {
+	case <-l.stop:
+		return ErrStopped
+	default:
+
+	}
+	select {
+	case <-l.stop:
+		return ErrStopped
+	case <-ctx.Done():
+		return ctx.Err()
+	case l.buckets <- struct{}{}:
+		go func() {
+			select {
+			case <-l.stop:
+			case <-time.After(l.interval):
+				<-l.buckets
+			}
+		}()
+		return nil
+	}
 }
 
 func (l *Limiter) Stop() {
-	panic("not implemented")
+	close(l.stop)
 }
