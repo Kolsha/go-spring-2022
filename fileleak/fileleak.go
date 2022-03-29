76776713c4e991d78a2ef8d1c97a716f157275bb
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-03-29 16:53:43 +0700

    first attempt to fileleak

--- fileleak/fileleak.go
+++ fileleak/fileleak.go
@@ -3,11 +3,36 @@
 
 package fileleak
 
+import (
+	"io/ioutil"
+	"log"
+	"reflect"
+)
+
 type testingT interface {
 	Errorf(msg string, args ...interface{})
 	Cleanup(func())
 }
 
+func getOpenedFileNames() []string {
+	files, err := ioutil.ReadDir("/proc/self/fd")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	names := make([]string, 0, len(files))
+	for _, file := range files {
+		names = append(names, file.Name())
+	}
+	return names
+}
+
 func VerifyNone(t testingT) {
-	panic("implement me")
+	before := getOpenedFileNames()
+	t.Cleanup(func() {
+		after := getOpenedFileNames()
+		if !reflect.DeepEqual(after, before) {
+			t.Errorf("there are file leaks")
+		}
+	})
 }
