cf8d3d290f70fc4995eddc2d813de183ca1609fc
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-04-18 13:07:29 +0300

    solved treeiter

--- treeiter/treeiter.go
+++ treeiter/treeiter.go
@@ -1,3 +1,15 @@
 //go:build !solution
 
 package treeiter
+
+func DoInOrder[E interface {
+	Left() *E
+	Right() *E
+}](root *E, cb func(t *E)) {
+	if root == nil {
+		return
+	}
+	DoInOrder((*root).Left(), cb)
+	cb(root)
+	DoInOrder((*root).Right(), cb)
+}
