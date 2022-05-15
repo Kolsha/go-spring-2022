d1c476e0bf85c8aec169b53e33039f28fd31eac9
Nikolai Ovchinnikov <kolsha-ru@yandex-team.ru>
2022-05-15 11:58:58 +0700

    sovled gzep

--- gzep/encode.go
+++ gzep/encode.go
@@ -5,11 +5,23 @@ package gzep
 import (
 	"compress/gzip"
 	"io"
+	"sync"
 )
 
-func Encode(data []byte, w io.Writer) error {
-	ww := gzip.NewWriter(w)
-	defer func() { _ = ww.Close() }()
+var pool sync.Pool
+
+func Encode(data []byte, dst io.Writer) error {
+	var ww *gzip.Writer
+	if w := pool.Get(); w != nil {
+		ww = w.(*gzip.Writer)
+		ww.Reset(dst)
+	} else {
+		ww, _ = gzip.NewWriterLevel(dst, gzip.DefaultCompression)
+	}
+	defer func() {
+		_ = ww.Close()
+		pool.Put(ww)
+	}()
 	if _, err := ww.Write(data); err != nil {
 		return err
 	}
